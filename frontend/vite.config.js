6a8c6161995f41b0680bc216a89c8a21c17252fd
shakeeb sa <shakeeb.sa.ahmed@gmail.com>
2026-02-02 17:16:43 +0500

    fix vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // If we're running on Vercel, use '/', otherwise use '/multi-links/' for GitHub Pages
-  base: process.env.VERCEL ? '/' : '/multi-links/',
+  base: '/multi-links/',
 })
